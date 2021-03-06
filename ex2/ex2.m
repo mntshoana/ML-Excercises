f4a89b8181d9bda7047f39acd2c7cf2ccc4ab8b5
Motsoaledi Neo Tshoana <mntshoana@outlook.com>
2021-03-06 22:21:44 +0200

    Beginning with excercise 2

---
+++ ex2/ex2.m
@@ -0,0 +1,22 @@
+%% Machine Learning - Exercise 2: Logistic Regression
+%
+% INITIALIZE
+clear;
+close all;
+clc;
+
+data = load('ex2data1.txt');
+X = data(:, [1, 2]);
+y = data(:, 3);
+% ====================   Part 1: Plotting   ====================
+fprintf(['Plotting data with + indicating (y = 1) examples and o ' ...
+         'indicating (y = 0) examples.\n']);
+
+plotData(X, y);
+hold on;
+xlabel('Exam 1 score')
+ylabel('Exam 2 score')
+legend('Admitted', 'Not admitted')
+hold off;
+fprintf('Paused. Press enter to continue.\n\n');
+pause;
