a25e974fcc8eef564b8b7e8f98367d8e9729b1cb
Motsoaledi Neo Tshoana <mntshoana@outlook.com>
2021-03-07 22:37:40 +0200

    Logistic regression cost function

---
+++ ex2/costFunction.m
@@ -0,0 +1,20 @@
+function [J, grad] = costFunction(theta, X, y)
+% Compute cost and gradient for logistic regression
+
+    m = length(y); % number of training examples
+    k = 1/m;
+    
+    % COST
+    z = X * theta;
+    h = sigmoid(z);
+    log1 = log(h);
+    log0 = log(1 - h);
+    a = - y' * log1;
+    b = (1 - y)' * log0;
+    J = k * (a - b);
+
+    % GRADIENT
+    d = X' * ( h - y );
+    grad = k * d;
+
+end
--- ex2/ex2.m
+++ ex2/ex2.m
@@ -20,3 +20,29 @@ legend('Admitted', 'Not admitted')
 hold off;
 fprintf('Paused. Press enter to continue.\n\n');
 pause;
+
+% ================ Part 2: Compute Cost and Gradient ================
+[m, n] = size(X);
+X = [ones(m, 1), X];
+
+%FILE: costFunction.m
+% Compute and display initial cost and gradient
+initial_theta = zeros(n + 1, 1);
+[cost, grad] = costFunction(initial_theta, X, y);
+fprintf('Cost at initial theta (zeros): %f\n', cost);
+fprintf('Expected cost (approx): 0.693\n');
+fprintf('Gradient at initial theta (zeros): \n');
+fprintf(' %f \n', grad);
+fprintf('Expected gradients (approx):\n -0.1000\n -12.0092\n -11.2628\n');
+
+% Compute and display cost and gradient with non-zero theta
+test_theta = [-24; 0.2; 0.2];
+[cost, grad] = costFunction(test_theta, X, y);
+fprintf('\nCost at test theta: %f\n', cost);
+fprintf('Expected cost (approx): 0.218\n');
+fprintf('Gradient at test theta: \n');
+fprintf(' %f \n', grad);
+fprintf('Expected gradients (approx):\n 0.043\n 2.566\n 2.647\n');
+
+fprintf('\nProgram paused. Press enter to continue.\n');
+pause;
