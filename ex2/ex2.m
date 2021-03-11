1bb81d0bcb9c79a99203e8a0595e7e5a62ce3afa
Motsoaledi Neo Tshoana <mntshoana@outlook.com>
2021-03-12 00:49:05 +0200

    Preparing For regularized part of excercise 2

--- ex2/ex2.m
+++ ex2/ex2.m
@@ -109,4 +109,4 @@ fprintf('Train Accuracy: %f\n', mean(double(p == y)) * 100);
 fprintf('Expected accuracy (approx): 89.0\n');
 fprintf('\n');
 fprintf('\nProgram paused. Press enter to close.\n');
-puase
+pause
