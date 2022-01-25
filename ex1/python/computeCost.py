241f5a14df35023f0678af23bfe20c0bd4e047e3
Motsoaledi Neo Tshoana <mntshoana@outlook.com>
2022-01-25 18:27:27 +0200

    Fixed bug with computCost in python implementation

--- ex1/python/computeCost.py
+++ ex1/python/computeCost.py
@@ -1,12 +1,15 @@
 import numpy as np
 def computeCost(X, y, theta):
 #   Cost for linear regression
+    print("X: ", X.shape)
+    print("y: ", y.shape)
     h = X.dot( theta )
-    a = np.subtract(h, y).T
-    b = np.subtract(h, y)
-    
+    a = (h - y).T
+    b = h - y
+    print("a: ", a.shape)
+    print("b: ", b.shape)
     m = h.shape[0] # Number of trainning examples
     mean = 1 / (2 * m)
-    
+    print("mean: ", mean)
     J = mean * (a.dot( b ))
     return J
--- ex1/python/ex1.py
+++ ex1/python/ex1.py
@@ -7,39 +7,27 @@ import pandas as pd
 import numpy as np
 os.system('cls' if os.name == 'nt' else 'clear')
 plt.close("all")
-## =================== Part 1: Basic Function ===================
-from warmUpExercise import warmUpExercise
-print('A basic function.')
-print(' - A 5x5 Identity Matrix: \n');
-# FILE: warmUpExercise.py
-print(warmUpExercise(), '\n')
-input('Paused. Press enter to continue.\n');
-
-## ===================    Part 2: Plotting   ====================
+## =================== Part 2: Plotting   ====================
 from plotData import plotData
-# x= population size in 10,000s
-# y= profit in $10,000s
+# X represents population size in units of 10,000s
+# y represents profit in units of $10,000s
 print('Plotting data')
 data = pd.read_csv('../ex1data1.txt', names=['Population', 'Profit'])
 X = data.iloc[:,0]
 y = data.iloc[:,1]
-m = data.shape[0] #tuple (row, col) // looking for row
 #FILE: plotData.py
 plotData(X,y)
-input('Paused. Press enter to continue.\n');
+input('Paused. Press enter to continue.\n')
 
 ## ============== Part 3: Cost and Gradient descent =============
 from computeCost import computeCost
 print('Testing the cost function ...')
-X = np.c_[np.ones((m,1)), data.iloc[:,0]]; #MX2
-y = np.c_[y];
-theta = np.zeros((2,1));                    #2X1
+m = data.shape #tuple (row, col)
+m = m[0] # row
+X = np.append(np.ones((m, 1)), data.iloc[:, 0].to_numpy().reshape(m,1), axis=1 )
+y = data.iloc[:, 1].to_numpy().reshape(m,1)
+theta = np.zeros((2,1))
 J = computeCost(X, y, theta)
-print(' - With theta = [0 ; 0]\nCost computed = ', J);
-print(' - Expected cost value (approx) 32.07');
-J = computeCost(X, y, np.c_[[-1 , 2]] );
-print('\n - With theta = [-1 ; 2]\nCost computed = ', J);
-print(' - Expected cost value (approx) 54.24');
-input('Paused. Press enter to continue.\n');
-
+print(' - With theta = [0 ; 0]\nCost computed = ', J)
+print(' - Expected cost value (approx) 32.07')
 ##=======================TODO PART 4 in python =======================
