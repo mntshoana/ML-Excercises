864db0f6e0a50e5239ba7182c8944437b6c25cf3
Motsoaledi Neo Tshoana <mntshoana@outlook.com>
2022-02-07 19:00:42 +0200

    Computed cost for logistc regression in octave

--- Logistic Regression/octave/costFunction.m
+++ Logistic Regression/octave/costFunction.m
@@ -8,6 +8,13 @@ function [J, grad] = costFunction(theta, X, y)
     %FILE: sigmoid.m
     h = sigmoid(z);
 
+    % COST
+    log1 = log(h);
+    log0 = log(1 - h);
+    a = - y' * log1;
+    b = (1 - y)' * log0;
+    J = 1/m * (a - b);
+
 
 
 end
