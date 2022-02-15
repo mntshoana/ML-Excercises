1dc1687aaa2eb72a3cf62f89b32c40764fab9dff
Motsoaledi Neo Tshoana <mntshoana@outlook.com>
2022-02-15 18:35:31 +0200

    Completed logistic regression excercise in octave

--- Logistic Regression/logisticRegression.m
+++ Logistic Regression/logisticRegression.m
@@ -87,4 +87,18 @@ pause;
 
 
 %% ============== Part 4: Predict and Accuracies ==============
-% TODO
+%  Predict probability for a student with score 45 on exam 1
+%  and score 85 on exam 2
+prob = sigmoid([1 45 85] * theta);
+fprintf(['For a student with scores 45 and 85, we predict an admission ' ...
+         'probability of %f\n'], prob);
+fprintf('Expected value: 0.775 +/- 0.002\n\n');
+
+% Compute accuracy on our training set
+p = predict(theta, X);
+
+fprintf('Train Accuracy: %f\n', mean(double(p == y)) * 100);
+fprintf('Expected accuracy (approx): 89.0\n');
+fprintf('\n');
+fprintf('\nProgram paused. Press enter to close.\n');
+pause
