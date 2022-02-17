9c9cfde3b2bc3491afbb9766f3b16d8b50eadd7a
Motsoaledi Neo Tshoana <mntshoana@outlook.com>
2022-02-17 06:52:13 +0200

    Preparing to implement cost function with regularization in octave

--- Logistic Regression/logisticRegression(regularization).m
+++ Logistic Regression/logisticRegression(regularization).m
@@ -21,4 +21,16 @@ fprintf('\nProgram paused. Press enter to continue.\n');
 pause;
 
 % ========== Regularized Logistic Regression ===========
+% Add Polynomial Features
+% FILE: polynomial.m
+X = polynomial(X(:,1), X(:,2));
+
+% Initialize fitting parameters
+initial_theta = zeros(size(X, 2), 1);
+
+% regularization parameter
+lambda = 1;
+
+% Compute and display initial cost and gradient for regularized logistic regression
+[cost, grad] = costFunctionReg(initial_theta, X, y, lambda);
 %TODO
---
+++ Logistic Regression/octave/costFunctionReg.m
@@ -0,0 +1,6 @@
+function [J, grad] = costFunctionReg(theta, X, y, lambda)
+    [Jfirst, gradfirst] = costFunction(theta, X, y);
+    % TODO
+    J = 1
+    grad = 1
+end
